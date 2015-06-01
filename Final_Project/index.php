9216bcdc5407981a32534aad1d999774440fa48b
Steven Teosin <StevenTeosin@users.noreply.github.com>
2015-06-01 12:27:14 +1000

    Create index.php

---
+++ Final_Project/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<div class="container">
+<head>
+<meta charset="utf-8">
+<title>Townsville Music - Home</title>
+<link href="mainstyles.css" rel="stylesheet" type="text/css">
+
+<body>
+  <header>
+   <img src="image/tcmc02.jpg" width="200" height="200" alt="Diggers logo"/>
+    <div class="login">
+    <?php 
+	include ("inc_headerLogin.php") ?>   
+    </div>
+</div>
+  <?php
+    
+     include ("inc_nav.php")  ?>;       
+  </header>  
+  <section id="content">
+  
+<div class="container">
+      
+  <h2>Welcome to Townsville Music Center Website</h2>
+  
+	<p>Based in Townsville, North Qld, the Music Centre presents concerts and workshops throughout the year, in a diverse range of genres including classical, jazz, folk, blues, world and contemporary music, featuring touring artists and locally-based professional and emerging artists.</p>
+
+	<p>Townsville Community Music Centre is a not-for-profit, incorporated organisation serving musicians and their audience in North Queensland. The Music Centre's role includes facilitation, presentation and promotion of events, functions, education, workshops, concerts, festivals in Townsville, and surrounding centres.
+	Based in Townsville, North Queensland, Australia, it promotes local artists and presents performances by inter- and intra-state and international artists.
+	It is involved in the instigation of projects based on cross collaboration with other artistic disciplines, and it promotes the work of North Queensland musicians outside the region, organising tours and promoting their work.
+	The Music Centre offers various levels of membership. To join and experience the benefits that membership provides when using this site please click here to join now. You can also join by printing out the form found at the bottom of this page.</p> 
+
+
+	<p>Individual membership for the Music Centre.
+	Music Centre Annual Membership Individual Subscription Option
+	Click here to <a href="subscribe.php">Subscribe</a>: $25.00 / 1Y </p>
+   <?php 
+   include ("inc_footer.php");
+    ?>
+   
+  </div>  
+</body>
+</html>
