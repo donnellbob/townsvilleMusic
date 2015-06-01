72c2dde0fea871d68bbc1bfbb330b61b9ec6ffe3
Steven Teosin <StevenTeosin@users.noreply.github.com>
2015-06-01 12:25:28 +1000

    Create hiring.php

---
+++ Final_Project/hiring.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<div class="container">
+<head>
+<meta charset="utf-8">
+<title>Townsville Music - Hiring</title>
+<link href="mainstyles.css" rel="stylesheet" type="text/css">
+
+<body>
+  <header>
+   <img src="image/tcmc02.jpg" width="200" height="200" alt="TCMC logo"/>
+    <div class="login">
+    <?php 
+	include ("inc_headerLogin.php") ?>   
+    </div>
+</div>     
+  <?php
+     include ("inc_nav.php");
+  ?>
+    </header>   
+   
+  <div class ="container">
+  <h1>Hiring</h1>
+  <p>So do you have some musical talent that you think people need to see? Do you want to show off? Do you want to get noticed by other musicians? Would you like to get hired and put your musical talent to work? Then this is the place for you.</p>
+
+  <p>Simply put your details in below and you can be on your way to getting payed to play!! </p> 
+      
+   <?php 
+   include ("inc_footer.php");
+    ?>
+   </div>
+    
+</body>
+</html>
