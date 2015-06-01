c9ae7c8c3fa2cfe160fdc13120a46db37a600331
Steven Teosin <StevenTeosin@users.noreply.github.com>
2015-06-01 12:20:02 +1000

    Create contactus.php

---
+++ Final_Project/contactus.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<div class="container">
+<head>
+<meta charset="utf-8">
+<title>Townsville Music - Contact Us</title>
+<link href="mainstyles.css" rel="stylesheet" type="text/css">
+
+<body>
+  <header>
+   <img src="image/tcmc02.jpg" width="200" height="200" alt="Diggers logo"/>
+    <div class="login">
+    <?php 
+	include ("inc_headerLogin.php") ?>   
+    </div>
+</div>      
+  <?php
+     include ("inc_nav.php");
+  ?>
+     </header>  
+   
+  <div class="container">
+  <h1>Contact Us</h1>
+  <h2>Postal Addres</h2>
+  <p>PO Box 1006, Townsville, Qld 4810
+ Address  Townsville Civic Theatre<br>
+ 41 Boundary Street, Townsville, Qld 4810 </p>
+  <h2>Phone</h2>
+    <p> Phone:   07 4724 2086<br>
+	Mobile    0402 255 182</p>
+ 
+  <h2>Email</h2>
+  <p><a href="mailto:e-mail admin@townsvillemusic.org.au">e-mail admin@townsvillemusic.org.au</a></p>
+    
+   <?php 
+   include ("inc_footer.php");
+    ?>
+   </div>
+    
+</body>
+</html>
