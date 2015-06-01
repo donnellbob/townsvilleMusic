1cbb4d107c93251dee30e0fe69b577c631c13092
Steven Teosin <StevenTeosin@users.noreply.github.com>
2015-06-01 12:24:12 +1000

    Create events.php

---
+++ Final_Project/events.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<div class="container">
+<head>
+<meta charset="utf-8">
+<title>Townsville Music - Events</title>
+<link href="mainstyles.css" rel="stylesheet" type="text/css">
+
+<body>
+  <header>
+   <img src="image/tcmc02.jpg" width="200" height="200" alt="Diggers logo"/>
+    <div class="login">
+    <?php 
+	include ("inc_headerLogin.php") ?>   
+    </div>
+</div>       
+  <?php
+     include ("inc_nav.php");
+  ?>
+       
+  </header>  
+  <div class ="container">
+  <h1>Events</h1>
+  <p>* SUNDAY VARIETY CONCERT Presented by Friends of the Theatre. 2pm Sunday 26 April at Pimlico Performing Arts Centre Fulham Road <b>Enquiries</b> - please phone 4723 7879</p>
+  <p>* Harbourside Duo presentsDream Serenade.Music of Debussy, Ravel, Saint- Saens, Sibelius and others.Dance the Habanera with the Girl with Flaxen Hair!</p>
+  <p>* Aviva String Quartet Following the phenomenal success of their 2014 season, the superb foursome return with their first concert for 2015, bringing their signature style of 		     classical and modern music to the masses in their own fresh and entertaining style.</p>
+  <p>* Townsville Concert Band 2pm Sunday 28 June at C2 (Townsville Civic Theatre)</p>
+  <p>* Pimlico choir Voices of Pimlico 2pm Sunday 6 September at C2 (Townsville Civic Theatre)</p>
+  <p>* Townsville Concert Band 2pm Sunday 27 September June at C2 (Townsville Civic Theatre)</p>
+  <p>* Allegro Choir 2pm Sunday 22 November at C2 (Townsville Civic Theatre)</p>
+  <p>*Aviva String Quartet The superb foursome return with their final concert for 2015, bringing their signature style of classical and modern music to the masses in their own fresh     and entertaining style. featuring Caroline Lloyd-Doolan on violin Susan Fraser on violin Jessica Winton on viola Ivy Wu on cello 2pm Sunday 29 November at C2 (Townsville Civic   Theatre) featuring Caroline Lloyd-Doolan on violin Susan Fraser on violin Jessica Winton on viola Ivy Wu on cello Ticketshop link. 2pm Sunday 14 June at C2 (CivicTheatre building)
+    $25 Adult - $20 Concession - Children 12 and under free! Be seduced by Thais! Or just relax and let the music wash over you. 2pm Sunday 17 May at C2 (Townsville Civic Theatre) 
+   March into Sommarhagen! 
+    
+    
+   <?php 
+   include ("inc_footer.php");
+    ?>
+   </div>
+    
+</body>
+</html>
